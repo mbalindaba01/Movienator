f9cf8def0d46813d80e6b183933981e588c3823d
Mbali Ndaba <mbalindaba01@gmail.com>
2020-01-09 14:54:51 +0200

    searchbar template fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MoviesComponent } from './movies/movies.component'
+import { MoviesComponent } from './movies/movies.component';
 import { FavouritesComponent } from './favourites/favourites.component';
 
 
